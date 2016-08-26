f7f5753a63b9e8bcc0feeb5c38f7e2903299d18b
Thomas A Caswell <tcaswell@gmail.com>
2016-08-26 11:09:46 -0400

    Initial commit

---
+++ setup.py
@@ -0,0 +1,20 @@
+from __future__ import (absolute_import, division, print_function)
+import versioneer
+
+import setuptools
+
+setuptools.setup(
+    name='',
+    version=versioneer.get_version(),
+    cmdclass=versioneer.get_cmdclass(),
+    license="BSD (3-clause)",
+    url="https://github.com/NSLS-II-ISS/isstools",
+    packages=setuptools.find_packages(),
+    package_data={'isstools': ['ui/*.ui']},
+    install_requires=['numpy', 'matplotlib'],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+    ],
+)
