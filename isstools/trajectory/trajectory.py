290a055b3f44b4dd52d24f636d3d1c6c9f94ebdb
Eli Stavitski <estavitski@Elis-iMac.home>
2016-09-13 23:11:38 -0400

    Solved problem with clamped spline using CubicSpline

--- isstools/trajectory/trajectory.py
+++ isstools/trajectory/trajectory.py
@@ -11,7 +11,7 @@ class trajectory():
 
 
     def build(self, edge_energy = 11564, offsets = ([-200,-30,50,1000]),velocities = ([200, 20, 200]), stitching = ([75, 75, 10, 10, 100, 100]),
-              servocycle = 16000, padding_lo = 1,padding_hi=1):
+              servocycle = 16000, padding_lo = 1 ,padding_hi=1):
         preedge_lo = edge_energy+offsets[0]
         preedge_hi = edge_energy+offsets[1]
         edge_lo = preedge_hi
@@ -31,13 +31,12 @@ class trajectory():
         postedge_stitch_lo = postedge_lo + stitching[4]
         postedge_stitch_hi = postedge_hi - stitching[5]
 
+        # create padding points to ensure derivative is zero
         t_padding_lo = 0
-        t_padding_1 = padding_lo / 4
-        t_padding_2 = padding_lo / 3
-        e_padding_lo = preedge_lo
+        e_padding_lo = preedge_lo-20
 
+        #create preedge
         t_current = padding_lo
-
         t_preedge_lo = t_current + (-preedge_lo + preedge_stitch_lo) / velocity_preedge
         e_preedge_lo = preedge_stitch_lo
         t_preedge_hi = t_current + (-preedge_lo + preedge_stitch_hi) / velocity_preedge
@@ -45,7 +44,6 @@ class trajectory():
 
 
         t_current = t_current + (-preedge_lo + preedge_hi) / velocity_preedge
-
         t_edge_lo = t_current + (-edge_lo + edge_stitch_lo) / velocity_edge
         e_edge_lo = edge_stitch_lo
         t_edge_hi = t_current + (-edge_lo + edge_stitch_hi) / velocity_edge
@@ -59,23 +57,43 @@ class trajectory():
         e_postedge_hi = postedge_stitch_hi
 
         t_current = t_current + (-postedge_lo + postedge_hi) / velocity_postedge
-        t_padding_hi = t_current + padding_hi
-        e_padding_hi = postedge_hi
 
-        self.time= np.array([t_padding_lo,t_padding_1,t_padding_2, t_preedge_lo, t_preedge_hi, t_edge_lo, t_edge_hi, t_postedge_lo, t_postedge_hi, t_padding_hi])
-        self.energy = np.array([e_padding_lo,e_padding_lo,e_padding_lo, e_preedge_lo, e_preedge_hi, e_edge_lo,e_edge_hi, e_postedge_lo, e_postedge_hi, e_padding_hi])
+        t_padding_hi = t_current + padding_hi
+        e_padding_hi = postedge_hi+20
+
+        # concatenate the arrays
+        self.time = np.array([t_padding_lo, t_preedge_lo, t_preedge_hi, t_edge_lo, t_edge_hi, t_postedge_lo, t_postedge_hi, t_padding_hi])
+        self.energy = np.array([e_padding_lo, e_preedge_lo, e_preedge_hi, e_edge_lo, e_edge_hi, e_postedge_lo, e_postedge_hi,e_padding_hi])
+
 
 
     def interpolate(self):
+        print(self.time)
+        print(self.energy)
+        cs = interpolate.CubicSpline(self.time,self.energy, bc_type='clamped')
+        self.time_grid = np.arange(self.time[0], self.time[-1], 1 / self.servocycle)
+        self.energy_grid=cs(self.time_grid)
 
+        """
         spl = interpolate.splrep(self.time,self.energy)
         self.time_grid = np.arange(self.time[0],self.time[-1],1/self.servocycle)
         self.energy_grid=interpolate.splev(self.time_grid,spl,der=0)
+        """
+    def cycle(self):
+        self.time_grid=np.append(self.time_grid,(self.time_grid+self.time_grid[-1]))
+        self.energy_grid=np.append(self.energy_grid,np.flipud(self.energy_grid))
+
+    def tile (self,reps = 1):
+            self.time_grid = np.tile(self.time_grid, reps)
+            self.energy_grid = np.tile(self.energy_grid, reps)
+
 
     def plot(self):
         plt.plot(self.time, self.energy, 'r+')
-        plt.plot(self.time_grid, self.energy_grid,'b')
+        plt.plot( self.energy_grid,'b')
         plt.show()
+
+
 
 
 
