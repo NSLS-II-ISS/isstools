bfeb1ee3b0158b9210e73216491a4f9c7403b84f
Eli on Mac <elistavitskI@gmail.com>
2016-09-20 18:48:01 -0400

    Added trajectory to the gui

--- isstools/gui.py
+++ isstools/gui.py
@@ -7,6 +7,8 @@ from matplotlib.backends.backend_qt4agg import (
     NavigationToolbar2QT as NavigationToolbar)
 import pkg_resources
 
+from isstools.trajectory.trajectory  import trajectory
+
 ui_path = pkg_resources.resource_filename('isstools', 'ui/XLive.ui')
 
 # def my_plan(dets, some, other, param):
@@ -31,6 +33,8 @@ class ScanGui(*uic.loadUiType(ui_path)):
         self.fig = fig = self.figure_content()
         self.addCanvas(fig)
         self.run_start.clicked.connect(self.test)
+        self.push_build_trajectory.clicked.connect(self.build_trajectory)
+        self.push_save_trajectory.clicked.connect(self.save_trajectory)
 
     def addCanvas(self, fig):
         self.canvas = FigureCanvas(fig)
@@ -54,6 +58,37 @@ class ScanGui(*uic.loadUiType(ui_path)):
         ax1f1.plot(np.random.rand(5))
         self.ax = ax1f1
         return fig1
+
+    def build_trajectory(self):
+        E0 = int(self.edit_E0.text())
+
+        preedge_lo = int(self.edit_preedge_lo.text())
+        preedge_hi = int(self.edit_preedge_hi.text())
+        edge_lo = preedge_hi
+        edge_hi = int(self.edit_edge_hi.text())
+        postedge_lo = edge_hi
+        postedge_hi = int(self.edit_postedge_hi.text())
+
+        velocity_preedge = int (self.velocity_preedge.text())
+        velocity_edge = int(self.velocity_edge.text())
+        velocity_postedge = int(self.velocity_postedge.text())
+
+        preedge_stitch_lo = int(self.preedge_stitch_lo.text())
+        preedge_stitch_hi = int(self.preedge_stitch_hi.text())
+        edge_stitch_lo =  int(self.edge_stitch_lo.text())
+        edge_stitch_hi = int(self.edge_stitch_hi.text())
+        postedge_stitch_lo = int(self.postedge_stitch_lo.text())
+        postedge_stitch_hi = int(self.postedge_stitch_hi.text())
+
+        padding_preedge = int(self.padding_preedge.text())
+        padding_postedge = int(self.padding_postedge.text())
+
+        traj=trajectory(edge_energy = E0, offsets = ([preedge_lo,preedge_hi,edge_hi,postedge_hi]),velocities = ([velocity_preedge, velocity_edge, velocity_postedge]),
+                        stitching = ([preedge_stitch_lo, preedge_stitch_hi, edge_stitch_lo, edge_stitch_hi, postedge_stitch_lo, postedge_stitch_hi]),
+                        servocycle = 16000, padding_lo = padding_preedge ,padding_hi=padding_postedge)
+
+    def save_trajectory(selfself):
+        pass
 
     def test(self):
         self.plan_func()
