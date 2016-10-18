372362d2b6a5fc16cd7f17048b2f06a3e84732f4
Bruno Luvizotto <luvizotto@bnl.gov>
2016-10-18 16:27:00 -0400

    Added methods to parse and plot fluorescence scans

--- isstools/xasdata/xasdata.py
+++ isstools/xasdata/xasdata.py
@@ -141,46 +141,91 @@ class XASdataFlu(XASdata):
 		self.i0 = np.array([])
 		self.trigger = np.array([])
 		self.iflu = np.array([])
-		self.it = np.copy(self.iflu)
+		self.it = np.array([])
+		self.ir = np.array([])
 		self.trig_file = ''
 
 	def process(self, encoder_trace = '', i0trace = '', iflutrace = '', trigtrace = ''):
-		self.load(encoder_trace, i0trace, iflutrace, trigtrace)
+		self.load(encoder_trace, i0trace, iflutrace,  trigtrace)
 		self.interpolate()
 		self.plot()
 
-	def load(self, encoder_trace = '', i0trace = '', iflutrace = '', trigtrace = ''):
+	def load(self, encoder_trace = '', i0trace = '', iflutrace = '',  trigtrace = ''):
 		self.encoder_file = encoder_trace
 		self.i0_file = i0trace
 		self.it_file = iflutrace
 		self.trig_file = trigtrace
 		self.encoder = self.loadENCtrace(encoder_trace)
-		self.energy = self.encoder
+		self.energy = np.copy(self.encoder)
 		self.energy[:, 1] = -12400 / (2 * 3.1356 * np.sin((np.pi / 180) * self.encoder[:, 1]/360000))
 		self.i0 = self.loadADCtrace(i0trace)
-		#self.trigger = self.loadTRIGtrace(trigtrace)
+		self.trigger = self.loadTRIGtrace(trigtrace)
 		self.iflu = self.loadADCtrace(iflutrace)
 		self.it = np.copy(self.iflu)
 
 	def interpolate(self):
-		min_timestamp = np.array([self.i0[0,0], self.it[0,0], self.encoder[0,0]]).max()
-		max_timestamp = np.array([self.i0[len(self.i0)-1,0], self.it[len(self.it)-1,0], self.encoder[len(self.encoder)-1,0]]).min()
-		interval = self.i0[1,0] - self.i0[0,0]
-		timestamps = np.arange(min_timestamp, max_timestamp, interval)
+		#self.trigger[1,1] / self.trigger[0,1]
+		i0_copy = np.copy(self.i0)
+		iflu_copy = np.copy(self.iflu)
+		energy_copy = np.copy(self.energy)
+		i0_interp = []
+		iflu_interp = []
+		energy_interp = []
+		trigger_interp = []
+		timestamps = []
+		#ir_copy = np.copy(self.ir)
 
-		#timestamps = self.trigger[:,0]
-		self.i0_interp = np.array([timestamps, np.interp(timestamps, self.i0[:,0], self.i0[:,1])]).transpose()
-		self.iflu_interp = np.array([timestamps, np.interp(timestamps, self.iflu[:,0], self.iflu[:,1])]).transpose()
-		self.it_interp = np.copy(self.iflu_interp)
-		self.energy_interp = np.array([timestamps, np.interp(timestamps, self.energy[:,0], self.energy[:,1])]).transpose()
+		for i in range(len(self.trigger) - 1):
 
-	def plot(self, color='r'):
-		result_chambers = self.i0_interp
+			condition1 = (self.trigger[i,0] <= i0_copy[:,0]) == (self.trigger[i + 1,0] > i0_copy[:,0])
+			interval1 = np.extract(condition1, i0_copy[:,0])
+
+			condition2 = (self.trigger[i,0] <= iflu_copy[:,0]) == (self.trigger[i + 1,0] > iflu_copy[:,0])
+			interval2 = np.extract(condition2, iflu_copy[:,0])
+
+			condition3 = (self.trigger[i,0] <= energy_copy[:,0]) == (self.trigger[i + 1,0] > energy_copy[:,0])
+			interval3 = np.extract(condition3, energy_copy[:,0])
+
+			if len(interval1) and len(interval2) and len(interval3):
+				interval_mean_i0 = np.mean(np.extract(condition1, i0_copy[:,1]))
+				i0_interp.append(interval_mean_i0)
+				i0_pos_high = np.where( i0_copy[:,0] == interval1[len(interval1) - 1] )[0][0]
+				i0_copy = i0_copy[i0_pos_high + 1:len(i0_copy)]
+
+
+				interval_mean_iflu = np.mean(np.extract(condition2, iflu_copy[:,1]))
+				iflu_interp.append(interval_mean_iflu)
+				iflu_pos_high = np.where( iflu_copy[:,0] == interval2[len(interval2) - 1] )[0][0]
+				iflu_copy = iflu_copy[iflu_pos_high + 1:len(iflu_copy)]
+
+
+				interval_mean_energy = np.mean(np.extract(condition3, energy_copy[:,1]))
+				energy_interp.append(interval_mean_energy)
+				energy_pos_high = np.where( energy_copy[:,0] == interval3[len(interval3) - 1] )[0][0]
+				energy_copy = energy_copy[energy_pos_high + 1:len(energy_copy)]
+
+				timestamps.append((self.trigger[i, 0] + self.trigger[i + 1, 0])/2)
+				trigger_interp.append(self.trigger[i, 1])
+
+		self.i0_interp = np.array([timestamps, i0_interp]).transpose()
+		self.iflu_interp = np.array([timestamps, iflu_interp]).transpose()
+		#self.it_interp = np.copy(self.iflu_interp)
+		self.energy_interp = np.array([timestamps, energy_interp]).transpose()
+		self.trigger_interp = np.array([timestamps, trigger_interp]).transpose()
+
+
+	def plot(self, ax=plt, color='r'):
+		result_chambers = np.copy(self.i0_interp)
 		result_chambers[:,1] = (self.iflu_interp[:,1] / self.i0_interp[:,1])
-		plt.plot(self.energy_interp[:,1], result_chambers[:,1], color)
-		plt.xlabel('Energy (eV)')
-		plt.ylabel('(iflu / i0)')
-		plt.grid(True)
+
+		ax.plot(self.energy_interp[:,1], result_chambers[:,1], color)
+		ax.grid(True)
+		if 'xlabel' in dir(ax):
+			ax.xlabel('Energy (eV)')
+			ax.ylabel('(iflu / i0)')
+		elif 'set_xlabel' in dir(ax):
+			ax.set_xlabel('Energy (eV)')
+			ax.set_ylabel('(iflu / i0)')
 
 	def export_trace(self, filename, filepath = '/GPFS/xf08id/Sandbox/', uid = ''):
 		suffix = '.txt'
@@ -196,10 +241,30 @@ class XASdataFlu(XASdata):
 			human_start_time = str(datetime.fromtimestamp(start_time).strftime('%m/%d/%Y  %H:%M:%S'))
 			human_stop_time = str(datetime.fromtimestamp(stop_time).strftime(' %m/%d/%Y  %H:%M:%S'))
 			human_duration = str(datetime.fromtimestamp(stop_time - start_time).strftime('%M:%S'))
-
+		
 		np.savetxt(fn, np.array([self.energy_interp[:,0], self.energy_interp[:,1], 
-					self.i0_interp[:,1], self.it_interp[:,1]]).transpose(), fmt='%17.6f %8.2f %f %f', 
-					delimiter=" ", header = 'Timestamp (s)   En. (eV) 	i0 (V)	  it(V)', comments = '# Year: {}\n# Cycle: {}\n# SAF: {}\n# PI: {}\n# PROPOSAL: {}\n# Scan ID: {}\n# UID: {}\n# Start time: {}\n# Stop time: {}\n# Total time: {}\n#\n# '.format(year, cycle, saf, pi, proposal, scan_id, real_uid, human_start_time, human_stop_time, human_duration))
+					self.i0_interp[:,1], self.iflu_interp[:,1]]).transpose(), fmt='%17.6f %8.2f %f %f', 
+					delimiter=" ", header = 'Timestamp (s)   En. (eV) 	i0 (V)	  iflu(V)', comments = '# Year: {}\n# Cycle: {}\n# SAF: {}\n# PI: {}\n# PROPOSAL: {}\n# Scan ID: {}\n# UID: {}\n# Start time: {}\n# Stop time: {}\n# Total time: {}\n#\n# '.format(year, cycle, saf, pi, proposal, scan_id, real_uid, human_start_time, human_stop_time, human_duration))
+		return fn
+
+	#def export_trace(self, filename, filepath = '/GPFS/xf08id/Sandbox/', uid = ''):
+	#	suffix = '.txt'
+	#	fn = filepath + filename + suffix
+	#	repeat = 1
+	#	while(os.path.isfile(fn)):
+	#		repeat += 1
+	#		fn = filepath + filename + '-' + str(repeat) + suffix
+	#	if(not uid):
+	#		pi, proposal, saf, comment, year, cycle, scan_id, real_uid, start_time, stop_time = '', '', '', '', '', '', '', '', '', ''
+	#	else:
+	#		pi, proposal, saf, comment, year, cycle, scan_id, real_uid, start_time, stop_time = db[uid]['start']['PI'], db[uid]['start']['PROPOSAL'], db[uid]['start']['SAF'], db[uid]['start']['comment'], db[uid]['start']['year'], db[uid]['start']['cycle'], db[uid]['start']['scan_id'], db[uid]['start']['uid'], db[uid]['start']['time'], db[uid]['stop']['time']
+	#		human_start_time = str(datetime.fromtimestamp(start_time).strftime('%m/%d/%Y  %H:%M:%S'))
+	#		human_stop_time = str(datetime.fromtimestamp(stop_time).strftime(' %m/%d/%Y  %H:%M:%S'))
+	#		human_duration = str(datetime.fromtimestamp(stop_time - start_time).strftime('%M:%S'))
+
+	#	np.savetxt(fn, np.array([self.energy_interp[:,0], self.energy_interp[:,1], 
+	#				self.i0_interp[:,1], self.it_interp[:,1]]).transpose(), fmt='%17.6f %8.2f %f %f', 
+	#				delimiter=" ", header = 'Timestamp (s)   En. (eV) 	i0 (V)	  it(V)', comments = '# Year: {}\n# Cycle: {}\n# SAF: {}\n# PI: {}\n# PROPOSAL: {}\n# Scan ID: {}\n# UID: {}\n# Start time: {}\n# Stop time: {}\n# Total time: {}\n#\n# '.format(year, cycle, saf, pi, proposal, scan_id, real_uid, human_start_time, human_stop_time, human_duration))
 
 	def export_trig_trace(self, filename, filepath = '/GPFS/xf08id/Sandbox/'):
 		np.savetxt(filepath + filename + suffix, self.energy_interp[:,1], fmt='%f', delimiter=" ")
