16ca1655ff2fd3c340052a6466cbde80eccf12aa
Bruno Luvizotto <luvizotto@bnl.gov>
2017-01-09 09:59:42 -0500

    Updated install_requires
    Removed packages that were previously provided

--- setup.py
+++ setup.py
@@ -11,7 +11,8 @@ setuptools.setup(
     url="https://github.com/NSLS-II-ISS/isstools",
     packages=setuptools.find_packages(),
     package_data={'isstools': ['ui/*.ui']},
-    install_requires=['numpy', 'matplotlib', 'netcdf4', 'pyqt', 'pyparsing'],
+    # install_requires=['numpy', 'matplotlib', 'netcdf4', 'pyqt', 'pyparsing'],
+    install_requires=['netcdf4','pyparsing'],
     classifiers=[
         "Development Status :: 3 - Alpha",
         "Programming Language :: Python :: 3.4",
