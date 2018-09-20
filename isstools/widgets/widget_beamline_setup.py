580e2a6701956f34dbb3273e7e0f6dce54a89411
Bruno Luvizotto <luvizotto@bnl.gov>
2018-09-20 11:37:39 -0400

    Fixing "update position saving" error

--- isstools/widgets/widget_beamline_setup.py
+++ isstools/widgets/widget_beamline_setup.py
@@ -903,7 +903,7 @@ class UIBeamlineSetup(*uic.loadUiType(ui_path)):
             # self.hhm.fb_pcoeff.put(self.piezo_kp)
 
             def update_piezo_params_plan(hhm, line, center, nlines,
-                                         nmeasures, pcoeff):
+                                         measures, pcoeff):
                 yield from bps.mv(hhm.fb_line, line,
                                   hhm.fb_center, center,
                                   hhm.fb_nlines, nlines,
