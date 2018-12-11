587452d5045b4340aa737fd3c570abe5a3b3510a
elistavitski <elistavitski@gmail.com>
2018-12-11 15:00:32 -0500

    Moved parameter grid logic to the parameter_handler

---
+++ isstools/elements/parameter_handler.py
@@ -0,0 +1,75 @@
+import inspect
+import re
+from PyQt5 import QtWidgets
+
+def parse_plan_parameters(plan_func):
+    def create_parameter(description, annotation):
+
+        param1 = None
+        param2 = None
+        def_val = ''
+        if description.find('=') != -1:
+            def_val = re.sub(r'.*=', '', description)
+        if annotation == int:
+            param1 = QtWidgets.QSpinBox()
+            param1.setMaximum(100000)
+            param1.setMinimum(-100000)
+            def_val = int(def_val)
+            param1.setValue(def_val)
+        elif annotation == float:
+            param1 = QtWidgets.QDoubleSpinBox()
+            param1.setMaximum(100000)
+            param1.setMinimum(-100000)
+            def_val = float(def_val)
+            param1.setValue(def_val)
+        elif annotation == bool:
+            param1 = QtWidgets.QCheckBox()
+            if def_val == 'True':
+                def_val = True
+            else:
+                def_val = False
+            param1.setCheckState(def_val)
+            param1.setTristate(False)
+        elif annotation == str:
+            param1 = QtWidgets.QLineEdit()
+            def_val = str(def_val)
+            param1.setText(def_val)
+
+        if param1 is not None:
+            param2 = QtWidgets.QLabel(description)
+
+        return param1, param2
+
+    parameter_values = []
+    parameter_descriptions = []
+    parameter_types = []
+
+    signature = inspect.signature(plan_func)
+    for i in range(0, len(signature.parameters)):
+
+        description = re.sub(r':.*?=', '=', str(signature.parameters[list(signature.parameters)[i]]))
+        if description == str(signature.parameters[list(signature.parameters)[i]]):
+            description = re.sub(r':.*', '', str(signature.parameters[list(signature.parameters)[i]]))
+        parameter_type = signature.parameters[list(signature.parameters)[i]].annotation
+        [parameter_value, parameter_description] = create_parameter(description, parameter_type)
+
+        if parameter_value:
+            parameter_values.append(parameter_value)
+            parameter_descriptions.append(parameter_description)
+            parameter_types.append(parameter_type)
+
+    return parameter_values, parameter_descriptions, parameter_types
+
+
+def  parse_grid_parameters(parameter_descriptions, parameter_values, parameter_types):
+    run_params = {}
+    for i in range(len(parameter_values)):
+        if (parameter_types[i] == int):
+            run_params[parameter_descriptions[i].text().split('=')[0]] = parameter_values[i].value()
+        elif (parameter_types[i] == float):
+            run_params[parameter_descriptions[i].text().split('=')[0]] = parameter_values[i].value()
+        elif (parameter_types[i] == bool):
+            run_params[parameter_descriptions[i].text().split('=')[0]] = bool(parameter_values[i].checkState())
+        elif (parameter_types[i] == str):
+            run_params[parameter_descriptions[i].text().split('=')[0]] = parameter_values[i].text()
+
--- isstools/widgets/widget_run.py
+++ isstools/widgets/widget_run.py
@@ -1,9 +1,6 @@
 import pkg_resources
-import inspect
-import re
-import os
-from subprocess import call
-from PyQt5 import uic, QtWidgets, QtCore
+
+from PyQt5 import uic, QtCore
 from matplotlib.backends.backend_qt5agg import (
     FigureCanvasQTAgg as FigureCanvas,
     NavigationToolbar2QT as NavigationToolbar)
@@ -12,21 +9,13 @@ import time as ttime
 import numpy as np
 import datetime
 from timeit import default_timer as timer
-from isstools.xiaparser import xiaparser
+
 from isstools.xasdata.xasdata import XASdataGeneric
 from isstools.elements.dialogs import question_message_box, message_box
 from isstools.elements.figure_update import update_figure
-
-
-# Libs needed by the ZMQ communication
-import json
-import pandas as pd
-
-timenow = datetime.datetime.now()
+from isstools.elements.parameter_handler import parse_plan_parameters
 
 ui_path = pkg_resources.resource_filename('isstools', 'ui/ui_run.ui')
-
-
 
 class UIRun(*uic.loadUiType(ui_path)):
     def __init__(self,
@@ -35,7 +24,7 @@ class UIRun(*uic.loadUiType(ui_path)):
                  RE,
                  db,
                  hhm,
-                 shutters,
+                 shutter_dictionary,
                  adc_list,
                  enc_list,
                  xia,
@@ -46,48 +35,32 @@ class UIRun(*uic.loadUiType(ui_path)):
         self.setupUi(self)
         self.addCanvas()
         # TODO : remove hhm dependency
-
-
         self.plan_funcs = plan_funcs
         self.plan_funcs_names = plan_funcs.keys()
         self.aux_plan_funcs = aux_plan_funcs
         self.RE = RE
         self.db = db
-        if self.db is None:
-            self.run_start.setEnabled(False)
-
-        self.shutters = shutters
+        self.shutter_dictionary = shutter_dictionary
         self.adc_list = adc_list
         self.enc_list = enc_list
         self.xia = xia
         self.gen_parser = XASdataGeneric(hhm.enc.pulses_per_deg, db)
         self.parent_gui = parent_gui
-
-        self.filepaths = []
-        self.xia_parser = xiaparser.xiaparser()
-
-        self.run_type.addItems(self.plan_funcs_names)
+        self.comboBox_scan_type.addItems(self.plan_funcs_names)
+        self.comboBox_scan_type.currentIndexChanged.connect(self.populate_parameter_grid)
         self.run_start.clicked.connect(self.run_scan)
-
-        self.pushButton_scantype_help.clicked.connect(self.show_scan_help)
-
-        self.run_type.currentIndexChanged.connect(self.populateParams)
-
         # List with uids of scans created in the "run" mode:
         self.run_mode_uids = []
 
-        self.params1 = []
-        self.params2 = []
-        self.params3 = []
-        if self.plan_funcs:
-            self.populateParams(0)
+        self.parameter_values = []
+        self.parameter_descriptions = []
+        self.populate_parameter_grid(0)
 
     def addCanvas(self):
         self.figure = Figure()
         self.figure.set_facecolor(color='#FcF9F6')
         self.canvas = FigureCanvas(self.figure)
         self.figure.ax1 = self.figure.add_subplot(111)
-
         self.figure.ax2 = self.figure.ax1.twinx()
         self.figure.ax3 = self.figure.ax1.twinx()
         self.toolbar = NavigationToolbar(self.canvas, self, coordinates=True)
@@ -100,8 +73,8 @@ class UIRun(*uic.loadUiType(ui_path)):
     def run_scan(self):
         ignore_shutter=False
 
-        for shutter in [self.shutters[shutter] for shutter in self.shutters if
-                        self.shutters[shutter].shutter_type != 'SP']:
+        for shutter in [self.shutter_dictionary[shutter] for shutter in self.shutter_dictionary if
+                        self.shutter_dictionary[shutter].shutter_type != 'SP']:
             if shutter.state.value:
                 ret = question_message_box(self,'Shutter closed',
                                            'Would you like to run the scan with the shutter closed?')
@@ -126,29 +99,29 @@ class UIRun(*uic.loadUiType(ui_path)):
                 self.xia.input_trigger.unit_sel.put(1)  # ms, not us
                 self.xia.input_trigger.period_sp.put(int(self.xia_samp_time))
 
-        self.comment = self.params2[0].text()
-        if (self.comment):
+        name_provided = self.parameter_values[0].text()
+        if name_provided:
             timenow = datetime.datetime.now()
             print('\nStarting scan at {}'.format(timenow.strftime("%H:%M:%S")))
             start_scan_timer=timer()
             
             # Get parameters from the widgets and organize them in a dictionary (run_params)
             run_params = {}
-            for i in range(len(self.params1)):
+            for i in range(len(self.parameter_values)):
                 if (self.param_types[i] == int):
-                    run_params[self.params3[i].text().split('=')[0]] = self.params2[i].value()
+                    run_params[self.parameter_descriptions[i].text().split('=')[0]] = self.parameter_values[i].value()
                 elif (self.param_types[i] == float):
-                    run_params[self.params3[i].text().split('=')[0]] = self.params2[i].value()
+                    run_params[self.parameter_descriptions[i].text().split('=')[0]] = self.parameter_values[i].value()
                 elif (self.param_types[i] == bool):
-                    run_params[self.params3[i].text().split('=')[0]] = bool(self.params2[i].checkState())
+                    run_params[self.parameter_descriptions[i].text().split('=')[0]] = bool(self.parameter_values[i].checkState())
                 elif (self.param_types[i] == str):
-                    run_params[self.params3[i].text().split('=')[0]] = self.params2[i].text()
+                    run_params[self.parameter_descriptions[i].text().split('=')[0]] = self.parameter_values[i].text()
 
 
             # Run the scan using the dict created before
             self.run_mode_uids = []
             self.parent_gui.run_mode = 'run'
-            plan_key = self.run_type.currentText()
+            plan_key = self.comboBox_scan_type.currentText()
             plan_func = self.plan_funcs[plan_key]
             self.run_mode_uids = self.RE(plan_func(**run_params,
                                                   ax=self.figure.ax1,
@@ -160,84 +133,28 @@ class UIRun(*uic.loadUiType(ui_path)):
             print('Scan duration {}'.format(stop_scan_timer-start_scan_timer))
 
         else:
-            print('\nPlease, type the name of the scan in the field "name"\nTry again')
+            message_box('No name provided', 'Please provide the name for the scan')
 
-    def show_scan_help(self):
-        title = self.run_type.currentText()
-        message = self.plan_funcs[title].__doc__
-        message_box(title, message)
+
 
     def create_log_scan(self, uid, figure):
         self.canvas.draw_idle()
         self.aux_plan_funcs['write_html_log'](uid, figure)
 
 
-    def populateParams(self, index):
-        plan_key = self.run_type.currentText()
-        plan_func = self.plan_funcs[plan_key]
-        for i in range(len(self.params1)):
-            self.gridLayout_13.removeWidget(self.params1[i])
-            self.gridLayout_13.removeWidget(self.params2[i])
-            self.gridLayout_13.removeWidget(self.params3[i])
-            self.params1[i].deleteLater()
-            self.params2[i].deleteLater()
-            self.params3[i].deleteLater()
-        self.params1 = []
-        self.params2 = []
-        self.params3 = []
-        self.param_types = []
+    def populate_parameter_grid(self, index):
+        for i in range(len(self.parameter_values)):
+            self.gridLayout_parameters.removeWidget(self.parameter_values[i])
+            self.gridLayout_parameters.removeWidget(self.parameter_descriptions[i])
+            self.parameter_values[i].deleteLater()
+            self.parameter_descriptions[i].deleteLater()
 
-        signature = inspect.signature(plan_func)
-        for i in range(0, len(signature.parameters)):
-            default = re.sub(r':.*?=', '=', str(signature.parameters[list(signature.parameters)[i]]))
-            if default == str(signature.parameters[list(signature.parameters)[i]]):
-                default = re.sub(r':.*', '', str(signature.parameters[list(signature.parameters)[i]]))
-            self.addParamControl(list(signature.parameters)[i], default,
-                                 signature.parameters[list(signature.parameters)[i]].annotation,
-                                 grid=self.gridLayout_13, params=[self.params1, self.params2, self.params3])
-            self.param_types.append(signature.parameters[list(signature.parameters)[i]].annotation)
+        plan_func = self.plan_funcs[self.comboBox_scan_type.currentText()]
+        [self.parameter_values, self.parameter_descriptions, self.list_param_types] = parse_plan_parameters(plan_func)
 
-    def addParamControl(self, name, default, annotation, grid, params):
-        rows = int((grid.count()) / 3)
-        param1 = QtWidgets.QLabel(str(rows + 1))
-
-        param2 = None
-        def_val = ''
-        if default.find('=') != -1:
-            def_val = re.sub(r'.*=', '', default)
-        if annotation == int:
-            param2 = QtWidgets.QSpinBox()
-            param2.setMaximum(100000)
-            param2.setMinimum(-100000)
-            def_val = int(def_val)
-            param2.setValue(def_val)
-        elif annotation == float:
-            param2 = QtWidgets.QDoubleSpinBox()
-            param2.setMaximum(100000)
-            param2.setMinimum(-100000)
-            def_val = float(def_val)
-            param2.setValue(def_val)
-        elif annotation == bool:
-            param2 = QtWidgets.QCheckBox()
-            if def_val == 'True':
-                def_val = True
-            else:
-                def_val = False
-            param2.setCheckState(def_val)
-            param2.setTristate(False)
-        elif annotation == str:
-            param2 = QtWidgets.QLineEdit()
-            def_val = str(def_val)
-            param2.setText(def_val)
-
-        if param2 is not None:
-            param3 = QtWidgets.QLabel(default)
-            grid.addWidget(param1, rows, 0, QtCore.Qt.AlignTop)
-            grid.addWidget(param2, rows, 1, QtCore.Qt.AlignTop)
-            grid.addWidget(param3, rows, 2, QtCore.Qt.AlignTop)
-            params[0].append(param1)
-            params[1].append(param2)
-            params[2].append(param3)
+        for i in range(len(self.parameter_values)):
+            self.gridLayout_parameters.addWidget(self.parameter_values[i], i, 0, QtCore.Qt.AlignTop)
+            self.gridLayout_parameters.addWidget(self.parameter_descriptions[i], i, 1, QtCore.Qt.AlignTop)
 
 
     def setAnalogSampTime(self, text):
