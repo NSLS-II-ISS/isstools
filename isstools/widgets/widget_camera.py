00f61083c24400c8f7239450d7d5b36b2d3e5a2b
elistavitski <elistavitski@gmail.com>
2020-06-25 13:19:40 -0400

    added 2 cameras and beam tracker with lines

--- isstools/widgets/widget_camera.py
+++ isstools/widgets/widget_camera.py
@@ -29,32 +29,93 @@ class UICamera(*uic.loadUiType(ui_path)):
         super().__init__(*args, **kwargs)
         self.setupUi(self)
         self.addCanvas()
+        self.cid = self.canvas_c1.mpl_connect('button_press_event', self.set_hcursor)
+        self.cid = self.canvas_c2.mpl_connect('button_press_event', self.set_vcursor)
+        self.h_vc = None
+        self.h_hc = None
+
         self.push_show_image.clicked.connect(self.show_image)
         self.camera_dict = camera_dict
 
 
-
+        self.timer_track_camera = QtCore.QTimer(self)
+        self.timer_track_camera.singleShot(0, self.track_camera)
+        self.timer_track_camera.setInterval(1000)
+        self.timer_track_camera.timeout.connect(self.track_camera)
+        self.timer_track_camera.start()
 
 
     def addCanvas(self):
-        self.figure = Figure()
-        self.figure.set_facecolor(color='#FcF9F6')
-        self.canvas = FigureCanvas(self.figure)
-        self.figure.ax = self.figure.add_subplot(111)
-        self.toolbar = NavigationToolbar(self.canvas, self, coordinates=True)
-        self.plots.addWidget(self.toolbar)
-        self.plots.addWidget(self.canvas)
-        #self.figure.ax3.grid(alpha = 0.4)
-        self.canvas.draw_idle()
+        self.figure_c1 = Figure()
+        self.figure_c1.set_facecolor(color='#FcF9F6')
+        self.canvas_c1 = FigureCanvas(self.figure_c1)
+        self.figure_c1.ax = self.figure_c1.add_subplot(111)
+
+        self.figure_c1.tight_layout()
+        self.toolbar_c1 = NavigationToolbar(self.canvas_c1, self, coordinates=True)
+        self.plot_camera1.addWidget(self.toolbar_c1)
+        self.plot_camera1.addWidget(self.canvas_c1)
+        self.canvas_c1.draw_idle()
+
+        self.figure_c2 = Figure()
+        self.figure_c2.set_facecolor(color='#FcF9F6')
+        self.canvas_c2 = FigureCanvas(self.figure_c2)
+        self.figure_c2.ax = self.figure_c2.add_subplot(111)
+
+        self.figure_c2.tight_layout()
+        self.toolbar_c2 = NavigationToolbar(self.canvas_c2, self, coordinates=True)
+        self.plot_camera2.addWidget(self.toolbar_c2)
+        self.plot_camera2.addWidget(self.canvas_c2)
+        self.canvas_c2.draw_idle()
 
     def show_image(self):
-        camera = self.camera_dict['camera_sample3']
-        print(camera)
-        image=camera.image.image
-        print(image)
-        a=self.figure.ax.imshow(image, cmap='gray')
-        print(a)
-        self.canvas.draw_idle()
+        if self.push_track.isChecked():
+            self.timer_track_camera.start()
+        else:
+            self.timer_track_camera.singleShot(0, self.track_camera)
+        camera1 = self.camera_dict['camera_sample1']
+        camera2 = self.camera_dict['camera_sample2']
+        image1 = camera1.image.image
+        image2 = camera2.image.image
+        self.figure_c1.ax.imshow(image1, cmap='gray')
+        self.figure_c2.ax.imshow(image2, cmap='gray')
+        self.canvas_c1.draw_idle()
+        self.canvas_c2.draw_idle()
+
+    def track_camera(self):
+        if not self.push_track.isChecked():
+            self.timer_track_camera.start()
+        else:
+            camera1 = self.camera_dict['camera_sample1']
+            camera2 = self.camera_dict['camera_sample2']
+            image1 = camera1.image.image
+            image2 = camera2.image.image
+            self.figure_c1.ax.imshow(image1, cmap='gray')
+            self.figure_c2.ax.imshow(image2, cmap='gray')
+            self.canvas_c1.draw_idle()
+            self.canvas_c2.draw_idle()
+
+    def set_vcursor(self, event):
+        if self.h_vc:
+            self.h_vc.remove()
+        if event.button == 3:
+            y1, y2 = self.figure_c2.ax.get_ylim()
+            self.h_vc = self.figure_c2.ax.vlines(event.xdata, y1,y2, color = 'green' )
+            self.canvas_c2.draw_idle()
+
+    def set_hcursor(self, event):
+
+        if event.button == 3:
+            if self.h_hc:
+                self.h_hc.remove()
+            x1, x2 = self.figure_c1.ax.get_xlim()
+            self.h_hc = self.figure_c1.ax.hlines(event.ydata, x1, x2, color = 'green' )
+            self.canvas_c1.draw_idle()
+
+
+
+
+
 
 
 
