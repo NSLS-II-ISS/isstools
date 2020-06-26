d2d39b2035c4fdb08d7d7f7f35807945b8fee167
elistavitski <elistavitski@gmail.com>
2020-06-26 09:11:58 -0400

    added qr reaing camera

--- isstools/widgets/widget_camera.py
+++ isstools/widgets/widget_camera.py
@@ -8,6 +8,7 @@ from matplotlib.backends.backend_qt5agg import (
     FigureCanvasQTAgg as FigureCanvas,
     NavigationToolbar2QT as NavigationToolbar)
 from matplotlib.figure import Figure
+import time
 from xas.xray import generate_energy_grid
 
 from isstools.dialogs.BasicDialogs import question_message_box, message_box
@@ -39,7 +40,6 @@ class UICamera(*uic.loadUiType(ui_path)):
 
 
         self.timer_track_camera = QtCore.QTimer(self)
-        self.timer_track_camera.singleShot(0, self.track_camera)
         self.timer_track_camera.setInterval(1000)
         self.timer_track_camera.timeout.connect(self.track_camera)
         self.timer_track_camera.start()
@@ -68,32 +68,44 @@ class UICamera(*uic.loadUiType(ui_path)):
         self.plot_camera2.addWidget(self.canvas_c2)
         self.canvas_c2.draw_idle()
 
+        self.figure_qr = Figure()
+        self.figure_qr.set_facecolor(color='#FcF9F6')
+        self.canvas_qr = FigureCanvas(self.figure_qr)
+        self.figure_qr.ax = self.figure_qr.add_subplot(111)
+
+        self.figure_qr.tight_layout()
+        self.toolbar_qr = NavigationToolbar(self.canvas_qr, self, coordinates=True)
+        self.plot_camera_qr.addWidget(self.toolbar_qr)
+        self.plot_camera_qr.addWidget(self.canvas_qr)
+        self.canvas_qr.draw_idle()
+
+
+    # def show_image(self, camera_):
+
+
     def show_image(self):
         if self.push_track.isChecked():
             self.timer_track_camera.start()
         else:
             self.timer_track_camera.singleShot(0, self.track_camera)
-        camera1 = self.camera_dict['camera_sample1']
-        camera2 = self.camera_dict['camera_sample2']
-        image1 = camera1.image.image
-        image2 = camera2.image.image
-        self.figure_c1.ax.imshow(image1, cmap='gray')
-        self.figure_c2.ax.imshow(image2, cmap='gray')
-        self.canvas_c1.draw_idle()
-        self.canvas_c2.draw_idle()
+        # time_in = time.time()
+
+        # print('time it took to draw figure:', time.time() - time_in)
 
     def track_camera(self):
-        if not self.push_track.isChecked():
-            self.timer_track_camera.start()
-        else:
-            camera1 = self.camera_dict['camera_sample1']
-            camera2 = self.camera_dict['camera_sample2']
-            image1 = camera1.image.image
-            image2 = camera2.image.image
-            self.figure_c1.ax.imshow(image1, cmap='gray')
-            self.figure_c2.ax.imshow(image2, cmap='gray')
-            self.canvas_c1.draw_idle()
-            self.canvas_c2.draw_idle()
+
+        camera1 = self.camera_dict['camera_sample1']
+        camera2 = self.camera_dict['camera_sample2']
+        camera_qr = self.camera_dict['camera_sample4']
+        image1 = camera1.image.image
+        image2 = camera2.image.image
+        image_qr = camera_qr.image.image
+        self.figure_c1.ax.imshow(image1, cmap='gray')
+        self.figure_c2.ax.imshow(image2, cmap='gray')
+        self.figure_qr.ax.imshow(image_qr, cmap='gray')
+        self.canvas_c1.draw_idle()
+        self.canvas_c2.draw_idle()
+        self.canvas_qr.draw_idle()
 
     def set_vcursor(self, event):
         if self.h_vc:
