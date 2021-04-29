547978dda9b0e24830789c21f25bb6717d6d5671
Thomas A Caswell <tcaswell@bnl.gov>
2021-04-28 20:27:32 -0400

    MNT/API: remove smbclient
    This is not used and correcting packaging sbmc for conda is hard (as
    it ends up depending on ldap and kerberos!)

--- isstools/xiaparser/xiaparser.py
+++ isstools/xiaparser/xiaparser.py
@@ -9,7 +9,6 @@ import h5py
 import os
 import os.path
 from scipy.optimize import curve_fit
-import smbc
 import time as ttime
 import warnings
 from pathlib import Path
@@ -118,7 +117,7 @@ class xiaparser:
             except Exception as exc:
                 print(exc)
                 return
-            
+
             print('Saving done!')
 
     def export_files_txt(self, dest_filepath, dest_filename = '', all_in_one = True):
@@ -143,14 +142,14 @@ class xiaparser:
                     print('XIA channel number: {}'.format(i))
                     i += 1
                     np.savetxt(f, np.array(row), fmt='%i',delimiter=' ', footer='============================================================')
-                
+
 
         else:
             for index, array in enumerate(exporting_arrays):
                 print('Creating file for channel {}...'.format(index))
                 np.savetxt('{}{}-chan{}.txt'.format(dest_filepath, tmpfilename, index), array, fmt='%i', delimiter=' ')
-      
-        
+
+
     def read_pixel_block(self, dataset, board_number, pixel, start_pos, plot_data, pixel_to_parse, silent):
         def read_statistics(ds, start):
             pos = start
@@ -164,7 +163,7 @@ class xiaparser:
             pos += 2
             output_events = self.two_word_to_int(ds[pos], ds[pos+1])
             return realtime, livetime, triggers, output_events
-    
+
         pos = start_pos #256 + (pixel + 1) *  #(pixel+1)*256
         tag_word_0 = dataset[pos+0]  # Tag Word 0: 0x33CC
         tag_word_1 = dataset[pos+1]  # Tag Word 0: 0xCC33
@@ -172,33 +171,33 @@ class xiaparser:
         mapping_mode = dataset[pos+3]  # 1 = Full Spectrum, 2 = Multiple ROI, 3 = List Mode
         pixel_number = self.two_word_to_int(dataset[pos+4], dataset[pos+5])
         total_pixel_block_size = self.two_word_to_int(dataset[pos+6], dataset[pos+7])
-        
+
         self.next_pos = start_pos + total_pixel_block_size
-    
+
         K = channel_0_size = dataset[pos+8]  # Channel 0 Size (K words)
         L = channel_1_size = dataset[pos+9]  # Channel 1 Size (L words)
         M = channel_2_size = dataset[pos+10]  # Channel 2 Size (M words)
         N = channel_3_size = dataset[pos+11]  # Channel 3 Size (N words)
-        
+
         reserved = dataset[pos+12:31]  # reserved (set to 0)
-        
+
         channel_0_stats_realtime, channel_0_stats_livetime, channel_0_stats_triggers, channel_0_stats_output_evs = read_statistics(dataset, pos+32)
         channel_1_stats_realtime, channel_1_stats_livetime, channel_1_stats_triggers, channel_1_stats_output_evs = read_statistics(dataset, pos+40)
         channel_2_stats_realtime, channel_2_stats_livetime, channel_2_stats_triggers, channel_2_stats_output_evs = read_statistics(dataset, pos+48)
         channel_3_stats_realtime, channel_3_stats_livetime, channel_3_stats_triggers, channel_3_stats_output_evs = read_statistics(dataset, pos+56)
-        
+
         reserved = dataset[pos+64:256]#255]  # reserved (set to 0)
-        
+
         channel_0_spectrum = dataset[pos+256:pos+256+K-1+1] # Python gets range - 1
         channel_1_spectrum = dataset[pos+256+K:pos+256+K+L-1+1]
         channel_2_spectrum = dataset[pos+256+K+L:pos+256+K+L+M-1+1]
-        channel_3_spectrum = dataset[pos+256+K+L+M:pos+256+K+L+M+N-1+1]    
+        channel_3_spectrum = dataset[pos+256+K+L+M:pos+256+K+L+M+N-1+1]
 
         self.chans[0].append(channel_0_spectrum)
         self.chans[1].append(channel_1_spectrum)
         self.chans[2].append(channel_2_spectrum)
         self.chans[3].append(channel_3_spectrum)
-    
+
         number = 0
         if pixel_to_parse is not None:
             number = np.uint64((pixel_to_parse + 1) or -1) - 1
@@ -211,35 +210,35 @@ class xiaparser:
             print("Mapping Mode:", mapping_mode)
             #print("Pixel Number:", pixel_number)
             print("Total Pixel Block Size:", total_pixel_block_size, "\n")
-        
+
             print("Channel {} Size: {}".format((4 * board_number) + 0, channel_0_size))
             print("Channel {} Realtime: {} ms".format((4 * board_number) + 0, channel_0_stats_realtime))
             print("Channel {} Livetime: {} ms".format((4 * board_number) + 0, channel_0_stats_livetime))
             print("Channel {} Triggers: {}".format((4 * board_number) + 0, channel_0_stats_triggers & 0xffff))
             print("Channel {} Output Events: {}".format((4 * board_number) + 0, channel_0_stats_output_evs & 0xffff))
             print("Channel {} Spectrum: {}\n".format((4 * board_number) + 0, channel_0_spectrum))
-        
+
             print("Channel {} Size: {}".format((4 * board_number) + 1, channel_1_size))
             print("Channel {} Realtime: {} ms".format((4 * board_number) + 1, channel_1_stats_realtime))
             print("Channel {} Livetime: {} ms".format((4 * board_number) + 1, channel_1_stats_livetime))
             print("Channel {} Triggers: {}".format((4 * board_number) + 1, channel_1_stats_triggers & 0xffff))
             print("Channel {} Output Events: {}".format((4 * board_number) + 1, channel_1_stats_output_evs & 0xffff))
             print("Channel {} Spectrum: {}\n".format((4 * board_number) + 1, channel_1_spectrum))
-        
+
             print("Channel {} Size: {}".format((4 * board_number) + 2, channel_2_size))
             print("Channel {} Realtime: {} ms".format((4 * board_number) + 2, channel_2_stats_realtime))
             print("Channel {} Livetime: {} ms".format((4 * board_number) + 2, channel_2_stats_livetime))
             print("Channel {} Triggers: {}".format((4 * board_number) + 2, channel_2_stats_triggers & 0xffff))
             print("Channel {} Output Events: {}".format((4 * board_number) + 2, channel_2_stats_output_evs & 0xffff))
             print("Channel {} Spectrum: {}\n".format((4 * board_number) + 2, channel_2_spectrum))
-        
+
             print("Channel {} Size: {}".format((4 * board_number) + 3, channel_3_size))
             print("Channel {} Realtime: {} ms".format((4 * board_number) + 3, channel_3_stats_realtime))
             print("Channel {} Livetime: {} ms".format((4 * board_number) + 3, channel_3_stats_livetime))
             print("Channel {} Triggers: {}".format((4 * board_number) + 3, channel_3_stats_triggers & 0xffff))
             print("Channel {} Output Events: {}".format((4 * board_number) + 3, channel_3_stats_output_evs & 0xffff))
             print("Channel {} Spectrum: {}".format((4 * board_number) + 3, channel_3_spectrum))
-        
+
             if plot_data:# and len(channel_0_spectrum):
                 print('plotting?')
                 fig = plt.figure()
@@ -251,14 +250,14 @@ class xiaparser:
                 ax.plot(channel_3_spectrum, label="Channel {} / RT: {} / LT: {}".format((4 * board_number) + 3, channel_3_stats_realtime, channel_3_stats_livetime))
                 plt.legend(loc="best")
                 plt.show()
-        
+
         return self.next_pos
 
 
     def two_word_to_int(self, wlow, whigh):
-        number = ((whigh & 0xFFFF) << 16) + (wlow & 0xFFFF) 
+        number = ((whigh & 0xFFFF) << 16) + (wlow & 0xFFFF)
         return number
-    
+
 
     def read_header(self, dataset, print_headers, silent):
         tag_word_0 = dataset[0]  # Tag Word 0: 0x55AA
@@ -269,10 +268,10 @@ class xiaparser:
         seq_buffer_number = self.two_word_to_int(dataset[5], dataset[6])  # Seq Buffer Number (low word first)
         buffer_id = dataset[7]  # BufferID (0: A, 1:B)
         number_of_pixels_in_buffer = dataset[8]  # Number of Pixels in buffer
-    
+
         starting_pixel_number = self.two_word_to_int(dataset[9], dataset[10])  # Starting Pixel Number (low word first)
         module_serial_number = dataset[11]  # Module Serial Number?/Module #
-    
+
         detector_channel_0 = dataset[12]  # Detector Channel 0 (set by host in DSP)
         detector_elem_channel_0 = dataset[13]  # Det. Element, Ch0
         detector_channel_1 = dataset[14]  # Detector Channel 1 (set by host in DSP)
@@ -281,17 +280,17 @@ class xiaparser:
         detector_elem_channel_2 = dataset[17]  # Det. Element, Ch2
         detector_channel_3 = dataset[18]  # Detector Channel 3 (set by host in DSP)
         detector_elem_channel_3 = dataset[19]  # Det. Element, Ch3
-    
+
         channel_0_size = dataset[20]  #Channel 0 Size (number of words)
         channel_1_size = dataset[21]  #Channel 1 Size (number of words)
         channel_2_size = dataset[22]  #Channel 2 Size (number of words)
         channel_3_size = dataset[23]  #Channel 3 Size (number of words)
-    
+
         buffer_errors = dataset[24]  # Buffer errors: Buffer overrun 0: No error / >0: Number of extra pixels combined with last pixel in buffer
         reserved = dataset[25:32]#31]  # Reserved (set to 0)
         user_words = dataset[32:64]#63]  # 32 User words (set in USER DSP array)
         reserved = dataset[64:256]#255]  # Reserved (set to 0)
-    
+
         if(print_headers and not silent):
             print("Tag Word 0:", format(tag_word_0 & 0xffff, '#06x')) #tag_work_0)
             print("Tag Word 1:", format(tag_word_1 & 0xffff, '#06x')) #tag_word_1)
@@ -317,7 +316,7 @@ class xiaparser:
             print("Channel 3 Size:", channel_3_size)
             print("Buffer Errors:", buffer_errors)
             print("User Words:", user_words)
-    
+
         return number_of_pixels_in_buffer
 
 
@@ -373,20 +372,20 @@ class xiaparser:
 
 
     def gain_matching(self, xia, center_energy, scan_range, channel_number, ax=plt):
-    
+
         center_energy = float(center_energy)
         scan_range = float(scan_range)
 
         graph_x = xia.mca_x.value
         graph_data = getattr(xia, "mca_array" + "{}".format(channel_number) + ".value")
-    
+
         condition = (graph_x <= (center_energy + scan_range)/1000) == (graph_x > (center_energy - scan_range)/1000)
         interval_x = np.extract(condition, graph_x)
         interval = np.extract(condition, graph_data)
 
         # p0 is the initial guess for fitting coefficients (A, mu and sigma)
         p0 = [.1, center_energy/1000, .1]
-        coeff, var_matrix = curve_fit(self.gauss, interval_x, interval, p0=p0) 
+        coeff, var_matrix = curve_fit(self.gauss, interval_x, interval, p0=p0)
         print('Intensity = ', coeff[0])
         print('Fitted mean = ', coeff[1])
         print('Sigma = ', coeff[2])
@@ -403,7 +402,7 @@ class xiaparser:
             ax.set_ylabel('Intensity')
 
         return coeff
-        
+
     def parse_step_scan(set_name, roi_start, roi_end, path = '/GPFS/xf08id/xia_files/'):
         det_channels = []
 
@@ -429,37 +428,3 @@ def frange(start, stop, step):
     while i < stop:
         yield i
         i += step
-
-class smbclient:
-    def __init__(self, filename = '', dest_filename = '', **kwargs):
-        self.ctx = smbc.Context()
-        self.ctx.optionNoAutoAnonymousLogin = True
-        self.ctx.functionAuthData = self.auth_fn
-        self.filename = filename # 'smb://elistavitski-ni/epics/elis_xia11_024.nc'
-        self.dest_filename = dest_filename
-
-    # Load different filenames (in case no filename was passed before)
-    def load(self, filename, dest_filename):
-        self.filename = filename
-        self.dest_filename = dest_filename
-
-    # Copy source file to dest file
-    def copy(self):
-        source_file = self.ctx.open(self.filename, os.O_RDONLY)
-        dest_file = open(self.dest_filename, 'wb')
-        dest_file.write(source_file.read())
-        dest_file.flush()
-        source_file.close()
-        dest_file.close()
-
-    def auth_fn(self, server, share, workgroup, username, password):
-        return ('xf08id-nas1', '08id-user', 'issuser08*')
-
-
-
-
-
-
-
-
-
