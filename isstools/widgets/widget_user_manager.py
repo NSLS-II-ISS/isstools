4eae025416c864abfaf06d341ac6b65351befe50
dleshchev <leshchev.denis@gmail.com>
2023-06-21 16:25:03 -0400

    setup user now forces the user info update in the widget_info_general
    Signed-off-by: elistavitski <elistavitski@gmail.com>

--- isstools/widgets/widget_user_manager.py
+++ isstools/widgets/widget_user_manager.py
@@ -238,6 +238,7 @@ class UIUserManager(*uic.loadUiType(ui_path)):
             self.populate_comboboxes()
             self.parent.widget_scan_manager.update_local_manager_list()
             self.parent.johann_emission.initialized = False
+            self.parent.widget_info_general.update_user_info()
 
 
 
