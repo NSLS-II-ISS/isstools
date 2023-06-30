8e8045888654fff89c7714f3be687b65a4b86800
dleshchev <leshchev.denis@gmail.com>
2023-06-30 13:47:52 -0400

    moved the resetting of the spectrometer to profile
    Signed-off-by: Eli Stavitski <istavitski@bnl.gov>

--- isstools/widgets/widget_user_manager.py
+++ isstools/widgets/widget_user_manager.py
@@ -237,7 +237,6 @@ class UIUserManager(*uic.loadUiType(ui_path)):
             self.cloud_setup(email_address=_email)
             self.populate_comboboxes()
             self.parent.widget_scan_manager.update_local_manager_list()
-            self.parent.johann_emission.initialized = False
             self.parent.widget_info_beamline.push_set_emission_energy.setEnabled(False)
             self.parent.widget_info_general.update_user_info()
 
